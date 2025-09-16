e872758a45537bdd3475298ddbdc3a3a2ec06223
rasikhikram <rasikhikram06@gmail.com>
2025-09-16 20:25:07 +0500

    Resolve Build Error

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from "@astrojs/vercel/static"; // ✅ add this
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  output: "static", // ✅ important for vercel static deploy
+  adapter: vercel(), // ✅ use vercel adapter
   integrations: [mdx(), sitemap(), vue()],
 
   vite: {
