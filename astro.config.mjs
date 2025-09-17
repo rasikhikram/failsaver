0aae49f9ab76476694008ce991d2ea9e176d1f39
rasikhikram <rasikhikram06@gmail.com>
2025-09-17 21:03:25 +0500

    Add Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,13 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   output: "static",
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), vue()],
 
   vite: {
