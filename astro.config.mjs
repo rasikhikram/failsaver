e7594e359db8e2dfa163f79ce715c35b2ffc0900
rasikhikram <rasikhikram06@gmail.com>
2025-09-17 20:50:08 +0500

    Add Quoatation in TipTap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,11 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   output: "static",
-  adapter: vercel(),
   integrations: [mdx(), sitemap(), vue()],
 
   vite: {
